2fc8708baac291f05fd0b872c20ec54e7fcae509
Chantha Voeun <cvoeun45@gmail.com>
2023-05-11 11:04:34 -0400

    Final

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-console.log("Checking if You want this Money")
+//Assign fetch to variable
+const fetchAPI = () => {
+    fetch("http://localhost:3000/mountains")
+    .then(response => response.json())
+    .then(places => addPlaces(places))
+    }
+    //Have to call function
+    fetchAPI()
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Stop Reload addEventListner
+const form = document.querySelector("form")
+form.addEventListener("submit",(e) =>{
+    e.preventDefault(),
+    console.log(e)})
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Create function to append with addEventListner
 const addPlaces = (dataArray) => {
-// console.log(dataArray)
+
+//getElementById and assign to variable
 const mountContainer = document.getElementById("mount-container")
-// console.log(mountContainer)
+
+//create forEach to Loop
 dataArray.forEach(element => {
-// console.log(element.img)
 const img = document.createElement('img')
 img.src = element.img
-// console.log(img)
-mountContainer.appendChild(img) 
-});
 
+//add Event Listner to click on img
+img.addEventListener("click", (e) => {
+const pTag = document.getElementById("p-Tag")
+const pTitle = document.createElement("p")
+pTitle.textContent = element.title
+pTag.append(pTitle)
+
+const pName = document.createElement("li")
+pName.textContent = element.name
+pTag.append(pName)
+
+const pCost = document.createElement("li")
+pCost.textContent = element.cost
+pTag.appendChild(pCost)
+})
+
+//append img
+mountContainer.appendChild(img)});
 };
 
 
 
-fetch("http://localhost:3000/mountains")
-.then(response => response.json())
-.then(places => addPlaces(places))
+
+
+
+
+
+
+
+
+
+
+
